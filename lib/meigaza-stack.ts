f291fff250326a283c05d2387fb142448bf4c2d3
fourside <fourside@gmail.com>
2020-08-23 22:06:02 +0900

    add lambdas and events and layer

--- lib/meigaza-stack.ts
+++ lib/meigaza-stack.ts
@@ -1,9 +1,46 @@
-import * as cdk from '@aws-cdk/core';
+import { Stack, StackProps, Construct } from '@aws-cdk/core';
+import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
+import { LayerVersion, AssetCode, Runtime } from "@aws-cdk/aws-lambda";
+import { Rule, Schedule } from "@aws-cdk/aws-events";
+import { LambdaFunction } from "@aws-cdk/aws-events-targets";
+import * as path from "path";
 
-export class MeigazaStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class MeigazaStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const layer = new LayerVersion(this, "chromeLayer", {
+      code: AssetCode.fromAsset(""), // TODO
+      compatibleRuntimes: [Runtime.NODEJS_12_X],
+    });
+
+    const meigazaFunction = new NodejsFunction(this, "meigaza", {
+      entry: path.join(__dirname, "lambdas/meigaza/maigaza.ts"),
+      runtime: Runtime.NODEJS_12_X,
+      layers: [layer],
+    });
+
+    new Rule(this, "meigazaRule", {
+      schedule: Schedule.cron({
+        minute: "0",
+        hour: "22",
+      }),
+      targets: [new LambdaFunction(meigazaFunction)],
+    });
+
+    const mvtkFunction = new NodejsFunction(this, "mvtk", {
+      entry: path.join(__dirname, "lambdas/mvtk/mvtk.ts"),
+      runtime: Runtime.NODEJS_12_X,
+      layers: [layer],
+    });
+
+    new Rule(this, "mvtkRule", {
+      schedule: Schedule.cron({
+        minute: "0",
+        hour: "22",
+        weekDay: "MON,WED",
+      }),
+      targets: [new LambdaFunction(mvtkFunction)],
+    });
   }
 }
