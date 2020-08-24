2584839801bdded9c44f2d22a0021cb368ee5f6c
fourside <fourside@gmail.com>
2020-08-24 13:10:28 +0900

    warn to kick env.sh before cdk command

--- bin/meigaza.ts
+++ bin/meigaza.ts
@@ -4,6 +4,10 @@ import * as cdk from '@aws-cdk/core';
 import { MeigazaStack } from '../lib/meigaza-stack';
 import { setUpLambdaLayer } from "../lib/setup-lambda-layer";
 
+if (!process.env.SLACK_INCOMING_WEBHOOK_URL) {
+  throw new Error("not set environment. execute `. .env.sh` before cdk command.");
+}
+
 setUpLambdaLayer();
 
 const app = new cdk.App();
