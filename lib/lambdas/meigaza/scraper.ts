a6d0f99f0a8c3f2005aad4ff32ac657401c1406f
fourside <fourside@gmail.com>
2022-01-05 09:19:08 +0900

    continue if failed one theater

--- lib/lambdas/meigaza/scraper.ts
+++ lib/lambdas/meigaza/scraper.ts
@@ -22,71 +22,75 @@ type ScraperResponse = {
   theater: Theater[];
 };
 
-export async function scraper(theaters: string[]): Promise<ScraperResponse> {
+export async function scraper(theaterNames: string[]): Promise<ScraperResponse> {
   let browser: Browser | undefined;
   try {
     browser = await browserLauncher();
-    const responses = [];
+    const theaters: Theater[] = [];
 
-    for (const theater of theaters) {
-      const page = await browser.newPage();
-      const url = `https://www.google.com/search?q=${encodeURIComponent(theater)}`;
-      await page.goto(url, { waitUntil: "domcontentloaded" });
-
-      const response: Theater = {
-        name: theater,
-        url: url,
-        schedules: [],
-      };
-
-      const dateList = await page.$$(".tb_tc li");
-      if (dateList.length === 0) {
-        throw new ScrapeError(theater, ".tb_tc li");
+    for (const theaterName of theaterNames) {
+      try {
+        const theater = await scrapeTheater(browser, theaterName);
+        theaters.push(theater);
+      } catch (error) {
+        console.error(`Failed to scrape ${theaterName}`, error);
       }
-      for (const dateElm of dateList) {
-        const dateText = await dateElm.getProperty("textContent");
-        if (dateText === undefined) {
-          throw new ScrapeError(theater);
-        }
-        const dateTextValue = await dateText.jsonValue<string>();
-        await dateElm.click();
-
-        const schedules = await page.$$(`[data-date="${dateTextValue}"] .lr_c_fcb.lr-s-stor`);
-        const movies: Movie[] = [];
-        for (const schedule of schedules) {
-          const titleElement = await schedule.$(".lr_c_tmt");
-          const title = await getTextFromElement(titleElement);
-          if (title === undefined) {
-            throw new ScrapeError(theater, ".lr_c_tmt");
-          }
-          const startElementList = await schedule.$$(".lr_c_s .lr_c_stnl");
-          const startList: string[] = [];
-          for (const startElm of startElementList) {
-            const startText = await startElm.getProperty("textContent");
-            if (startText === undefined) {
-              throw new ScrapeError(theater, ".lr_c_s .lr_c_stnl");
-            }
-            const startTextValue = await startText.jsonValue<string>();
-            startList.push(startTextValue);
-          }
-
-          movies.push({
-            title,
-            startAt: startList,
-          });
-        }
-
-        response.schedules.push({
-          date: dateTextValue,
-          movies: movies,
-        });
-      }
-      responses.push(response);
     }
-    return { theater: responses };
+    return { theater: theaters };
   } finally {
     if (browser) {
       await browser.close();
     }
   }
+}
+
+async function scrapeTheater(browser: Browser, theaterName: string): Promise<Theater> {
+  const page = await browser.newPage();
+  const url = `https://www.google.com/search?q=${encodeURIComponent(theaterName)}`;
+  await page.goto(url, { waitUntil: "domcontentloaded" });
+
+  const theater: Theater = {
+    name: theaterName,
+    url: url,
+    schedules: [],
+  };
+
+  const dateList = await page.$$(".tb_tc li");
+  if (dateList.length === 0) {
+    throw new ScrapeError(theaterName, ".tb_tc li");
+  }
+  for (const dateElm of dateList) {
+    const dateText = await dateElm.getProperty("textContent");
+    if (dateText === undefined) {
+      throw new ScrapeError(theaterName);
+    }
+    const dateTextValue = await dateText.jsonValue<string>();
+    await dateElm.click();
+
+    const schedules = await page.$$(`[data-date="${dateTextValue}"] .lr_c_fcb.lr-s-stor`);
+    const movies: Movie[] = [];
+    for (const schedule of schedules) {
+      const titleElement = await schedule.$(".lr_c_tmt");
+      const title = await getTextFromElement(titleElement);
+      if (title === undefined) {
+        throw new ScrapeError(theaterName, ".lr_c_tmt");
+      }
+      const startElementList = await schedule.$$(".lr_c_s .lr_c_stnl");
+      const startList: string[] = [];
+      for (const startElm of startElementList) {
+        const startText = await startElm.getProperty("textContent");
+        if (startText === undefined) {
+          throw new ScrapeError(theaterName, ".lr_c_s .lr_c_stnl");
+        }
+        const startTextValue = await startText.jsonValue<string>();
+        startList.push(startTextValue);
+      }
+
+      movies.push({
+        title,
+        startAt: startList,
+      });
+    }
+  }
+  return theater;
 }
